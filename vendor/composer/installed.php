286d2b5f85e6057f1d9c2fdfc8a1c73df7d99c71
Reagan Mahinay <reaganmahinay.web@gmail.com>
2026-01-31 03:57:50 +0800

    Add Composer autoloading and class management files
    - Created autoload.php for Composer autoloading setup.
    - Added ClassLoader.php to implement PSR-0, PSR-4, and classmap loading.
    - Introduced InstalledVersions.php to manage installed package versions.
    - Included LICENSE file for Composer package licensing.
    - Generated autoload_classmap.php for class mapping.
    - Created autoload_namespaces.php for namespace autoloading.
    - Added autoload_psr4.php for PSR-4 autoloading configuration.
    - Implemented autoload_real.php for real autoloading functionality.
    - Added autoload_static.php for static autoloading data.
    - Created installed.json to track installed packages.
    - Generated installed.php to manage package installation data.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'personal/himuon-image-metadata-ai',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'df9b829714e812504210f91852afc1ce385acb53',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'personal/himuon-image-metadata-ai' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'df9b829714e812504210f91852afc1ce385acb53',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
